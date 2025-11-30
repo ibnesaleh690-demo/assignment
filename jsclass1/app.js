c1de19ce09af598ebb801569e6d79cd29e352edd
ibnesaleh690-demo <ibnesaleh690@mail.com>
2025-11-30 13:05:10 +0500

    js assignment

---
+++ jsclass1/app.js
@@ -0,0 +1,25 @@
+var username = 'ibnesaleh'; 
+var myName = 'Saleh'
+document.write ('ibnesaleh' + '</br>')
+document.write (myName + '</br>')
+var A = 'Hello World'
+
+
+alert (A)
+
+// var studentNam = 'M saleh'
+// var studentAge = '18 old years'
+// var stundentCun = 'pakistan'
+// var stundentEdu = 'interpass'
+// alert (studentNam)
+// alert(studentAge)
+// alert(stundentCun)
+// alert(stundentEdu)
+ var email = "ibnesaleh0@gmail";
+ alert("My email address is  " + email)
+ var C = "you will be a web developer in pakistan, and married to amna with 2 kids ";
+ alert(C)
+var D = "A smarter way to learn javascript "
+alert(D)
+var F = "Yay! I can write HTML content through javascript "
+document.write(F)
