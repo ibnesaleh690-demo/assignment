114651176ca2ee00c549c09b1b959b44b9a65df4
ibnesaleh690-demo <ibnesaleh690@mail.com>
2025-12-03 07:53:11 +0500

    assign4

---
+++ jsclsmathEx/app.js
@@ -0,0 +1,114 @@
+//Q1
+
+// var num1 = 10
+// var num2 = 98
+// var res =(num1+num2)
+// document.write('10 + 98 :'+ res + "<br>")
+
+//Q2
+
+// var num3 = 10
+// var num4 = 98
+// var res= (num3-num4)
+// document.write("10 - 98 :" + res + "<br>")
+
+// var num5 = 110
+// var num6 = 98
+// var res= (num5%num6)
+// document.write("110 % 98 :" + res + "<br>")
+
+// var num7 = 100
+// var num8 = 97
+// var res = (num7*num8)
+// document.write("100 * 97: " + res)
+
+//Q5
+
+// var num1 = 4
+// var num2 = 1
+// var res = (num1*num2)
+// document.write("4 * 1 :   "+ res + "<br>")
+// var num3 = 4
+// var num4 = 2
+// var res = (num3*num4)
+// document.write("4 * 2 :   "+ res + "<br>")
+// var num5 = 4
+// var num6 = 3
+// var res = (num5*num6)
+// document.write("4 * 3 :   "+ res + "<br>")
+// var num7 = 4
+// var num8 = 4
+// var res = (num7*num8)
+// document.write("4 * 4 :   "+ res + "<br>")
+// var num9 = 4
+// var num10 = 5
+// var res = (num9*num10)
+// document.write("4 * 5 :   "+ res + "<br>")
+// var num11 = 4
+// var num12 = 6
+// var res = (num11*num12)
+// document.write("4 * 6 :   "+ res + "<br>")
+// var num13 = 4
+// var num14 = 7
+// var res = (num13*num14)
+// document.write("4 * 7 :   "+ res + "<br>")
+// var num15 = 4
+// var num16 = 8
+// var res = (num15*num16)
+// document.write("4 * 8 :   "+ res + "<br>")
+// var num17 = 4
+// var num18 = 9
+// var res = (num17*num18)
+// document.write("4 * 9 :   "+ res + "<br>")
+// var num19 = 4
+// var num20 = 10
+// var res = (num19*num20)
+// document.write("4 * 10 :   "+ res + "<br>")
+
+//Q7
+
+// var item1 = 590
+// var item1Q = 2
+// var res =(item1*item1Q)
+// var item2 = 150
+// var item2Q = 5
+// var shipping = 150
+// var res1 = (item2*item2Q+shipping)
+// document.write(res+res1)
+
+//Q8
+
+// var totalMarks = 1200
+// var Marksobtained =900
+// document.write("Total Marks: ",totalMarks+"<br>"+"Marks obtained: ", Marksobtained + "<br>")
+// var TotalMarks = 1200
+// var MarksObtained =900
+// var res = ( MarksObtained / TotalMarks  * 100)
+// document.write("Percentage : ", res)
+
+//Q9
+
+// var us1 = 10
+// var pak = 104
+// var res = (us1*pak)
+
+// var sau = 25
+// var pak1 = 28
+// var res1 = (sau*pak1)
+// var totalRes =(res+res1)
+
+// document.write("Converted the total currency to pakistan rupees :" + totalRes)
+
+
+//Q10
+
+// var num = 5 * 10 % 2
+// var res =(num)
+// alert(res)
+
+//Q11
+
+var crntYear = 2025
+var birthYear = 2006
+var res = (crntYear-birthYear)
+document.write("they are either   " +res + " or 20 years old")
