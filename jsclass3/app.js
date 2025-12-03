9014f2cdb3ca4545376663f68c67570c18d8b014
ibnesaleh690-demo <ibnesaleh690@mail.com>
2025-12-03 13:06:43 +0500

    legal&illegal

---
+++ jsclass3/app.js
@@ -0,0 +1,27 @@
+//  let a = 10, b = 20 , c = 30;
+//  document.write(a+b+c)
+
+// legal variables
+
+//  var  myVaribal = 10
+//  var  LET = 29
+//  var  Colspane = 56
+//  var  Alert = 88
+//  var  ALERT = 99
+
+ // illegal variables
+ 
+//  var alert = 10
+//  var variable = 90
+//  var colspane = 80
+//  var document = 100
+//  var let = 98
+
+//  document.write(alert+variable+colspane+document-let)
+
+var A = "Variables names can olny contain , latters,numbers, doller sign, and underscores likely whithout any space "
+var exm ="For example : $my, first-variable"
+var B = "Variables must begin with a dollar sign, underscore, latters"
+var C = "varible name are case camel"
+var D = "Variable names should not be js keyword", F="for example: colspane" 
+document.write(A+"<br>"+"<br>"+exm +"<br>"+"<br>"+B+"<br>"+"<br>"+C+"<br>"+"<br>"+D+"<br>"+"<br>"+F)
