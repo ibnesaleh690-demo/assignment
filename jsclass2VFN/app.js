f1fc5275fc98e020b500f5f103a71081ec505042
ibnesaleh690-demo <ibnesaleh690@mail.com>
2025-12-03 07:51:59 +0500

    assign3

---
+++ jsclass2VFN/app.js
@@ -0,0 +1,12 @@
+var age = 'I ma 19 years old'
+alert(age)
+var visit = "you have visited this site 19 times"
+alert(visit)
+// var birth = "My birth is 2006 "
+// var brt = ("Data type of my decleard variable is number")
+// document.write (birth+ '</br>')
+// document.write (brt)
+var name = "ali khan"
+var product = "ordered 3shoes"
+var store = "on Daraz" 
+document.write(name+" "+product+" "+store)
