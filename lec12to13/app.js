7fa113aae08c0fb23c6a401b0ff252a2793769fb
ibnesaleh690-demo <ibnesaleh690@mail.com>
2025-12-20 17:06:46 +0500

    cheptar 12 to 13

---
+++ lec12to13/app.js
@@ -0,0 +1,246 @@
+//Q1
+
+// var num = +prompt("Enter number")
+// if(num % 3 === 0){
+//     alert(num +" is devesible by 3")
+// }else{
+//     alert(num +" is not devesible by 3")
+// }
+
+//Q2
+
+// var number = +prompt("Enter number ")
+//  if(number % 2 === 0){
+//    alert("The number is EVEN")
+//  }else{
+//     alert("The number is ODD")
+//  }
+
+//Q3
+
+// var age = +prompt("Enter your age")
+
+// if(age >= 18 ){
+//     alert("Old enough")
+// }else{
+//     alert("Too young")
+// }
+
+
+//Q4
+
+
+
+
+
+
+
+//Q5
+
+//  var num = +prompt("Enter number")
+// if(num % 3 === 0){
+//     alert(num +" is devesible by 3")
+// }else{
+//     alert(num +" is not devesible by 3")
+// }
+
+//Q6
+
+// let ch = prompt("Enter a character:");
+
+// let ch2 = ch.charCodeAt(0);
+
+// if (ch2 >= 48 && ch2 <= 57) {
+//     alert("it's a number");
+// }
+// else if (ch2 >= 65 && ch2 <= 90) {
+//     alert("It's a  UPPERCASE letter");
+// }
+// else if (ch2 >= 97 && ch2 <= 122) {
+//     alert("It's a lowercase letter");
+// }
+// else {
+//     console.log("It's wrong");
+// }
+
+
+//Q7
+
+// var num1 = +prompt("Enter first number")
+// var num2 = +prompt("Enter second number")
+// var operat = prompt("Enter operatores  (+,-,/,*)")
+
+// var calcu = 0;
+
+// if(operat === "+"){
+//     operat = num1+num2
+// }else if (operat === "-"){
+//     operat = num1-num2
+// }else if (operat === "*"){
+//     operat = num1*num2
+// }else if (operat === "/"){
+//     operat = num1/num2
+// }else {
+//     alert('Enter write number')
+
+// }
+// document.write(operat)
+
+//Q8
+
+// var time = +prompt("Enter time")
+
+
+// if( time >= 0 && time <= 1200){
+//    alert("Good Morning")
+// }else if ( time >= 1200 && time <= 1700){
+//     alert("Good aftermoon")
+// }else if ( time >= 1700 && time <= 1900){
+//     alert("Good evening")
+// }else if ( time >= 2100 && time <= 2359){
+//     alert("Good night")
+// }
+
+//Q9
+
+// var year = +prompt("Enter yrear")
+
+// if(year % 4 === 0){
+//     alert("It's leap year")
+// }else if (year % 400 === 0){
+//     alert("It's  leap year")
+// }else if (year % 100 === 0){
+//     alert("It's not leap year")
+// }else{
+//     alert("It's not leap year")
+// }
+
+//Q10
+
+// var validPass = "abcd1234"
+// var userPass = prompt("Enter password")
+// if(userPass === validPass){
+//     alert(" “Correct! The password you entered matches the original password")
+// }else if (userPass === ""){
+//     alert(" Please enter your password")
+// }else{
+//     alert("“Incorrect password")
+// }
+
+//Q12
+
+// var greeting = 1800; 
+// var hour = 1759; 
+// if (hour <= greeting ) { 
+// alert(greeting = "Good day"); 
+ 
+// } else {
+// alert(greeting = "Good evening");
+// }
+
+//Q13
+
+// var num1 = +prompt("Enter first number")
+// var num2 = +prompt("Enter second number")
+ 
+// if(num1 < num2 ){
+//     alert("NUM2 is larger ")
+// }else if (num1 > num2){
+//     alert("NUM1 is larger ")
+// }else{
+//     alert("Both are equal")
+// }
+
+//Q14
+
+// var num = +prompt("Enter a number")
+
+// if(num > 0){
+//     alert("It's positive")
+// }else if(num < 0){
+//     alert("It's negative")   
+// }else if(num >= 0 && num <= 0){
+//     alert("It's zore")
+// }
+
+//Q15
+
+// var hour = +prompt("Enter current hour")
+
+// if(hour >= 6 && hour <= 9 ){
+//     alert("Breakfast is served")
+// }else if (hour >= 11 && hour <= 13){
+//     alert("It's time for lunch")
+// }else if (hour >= 17 && hour <= 20){
+//     alert("It's time for dinner")
+// }else{
+//     alert("Sorry, you'll have to wait, or go get a snack")
+// }
+
+ //Q16
+
+//  var ver1 = 29;
+//  console.log(typeof ver1)
+//  var ver2 = false
+//  console.log(typeof ver2)
+// var ver3 = "karachi"
+//  console.log(typeof ver3)
+// var ver4 = 
+//  console.log(typeof ver4)
+
+// var ver1 = 29;
+// var ver2 = false
+// var ver3 = "karachi"
+// var ver4 = 
+ 
+// console.log(typeof ver1  + typeof ver2 + typeof ver3 + typeof ver4)
+
+//Q17
+
+// var character = prompt("Enter a character")
+// if(character === "a" || character === "e" || character === "i" ||      character === "o" || character === "u"){
+//   alert("true")
+// }else{
+//     alert("false")
+// }
+ 
+//Q18
+
+//  == 
+
+//Q19
+
+
+// var month = +prompt("Enter month number")
+
+// if(month == 1 ){
+//     alert("january")
+// }else if (month == 2){
+//     alert("february")
+// }else if (month == 3){
+//     alert("march")
+// }else if (month == 4){
+//     alert("april")
+// }else if (month == 5){
+//     alert("may")
+// }else if (month == 6){
+//     alert("june")
+// }else if (month == 7){
+//     alert("july")
+// }else if (month == 8){
+//     alert("august")
+// }else if (month == 9){
+//     alert("september")
+// }else if (month == 10){
+//     alert("october")
+// }else if (month == 11){
+//     alert("november")
+// }else if (month == 12){
+//     alert("december")
+// }
+// else{
+//     alert("invalid month")
+// }
+ 
+
+
